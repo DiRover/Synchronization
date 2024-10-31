57a3ff93dfe741bf75f565ef2b910dd1ce12b8af
Dmitrij Rovenskij <rovenskij@element-soft.com>
2024-10-31 15:38:54 +0300

    freqlog

--- src/main/java/di_rover/Main.java
+++ src/main/java/di_rover/Main.java
@@ -5,11 +5,54 @@ import java.util.*;
 public class Main {
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        ThreadGroup group = getThreadGroup();
+        ThreadGroup group = new ThreadGroup("Group");
 
+        Thread maxCheckThread = new Thread(() -> {
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    Map.Entry<Integer, Integer> mostFrequentEntry = sizeToFreq.entrySet().stream()
+                            .max(Map.Entry.comparingByValue())
+                            .orElse(null);
+
+                    if (mostFrequentEntry != null) {
+                        System.out.println("Current max " + mostFrequentEntry.getKey() +
+                                " (встретилось " + mostFrequentEntry.getValue() + " раз)");
+                    }
+
+                }
+            }
+        });
+
+        maxCheckThread.start();
+
+        for (int i = 0; i < 1000; i++) {
+            Thread thread = new Thread(group, () -> {
+                String route = generateRoute("RLRFR", 100);
+                int repeats = countRInRoute(route);
+
+                synchronized (sizeToFreq) {
+                    sizeToFreq.merge(repeats, 1, Integer::sum);
+                    sizeToFreq.notify();
+                }
+
+            });
+
+            thread.start();
+        }
+
+
+        maxCheckThread.interrupt();
         group.interrupt();
+
+        maxCheckThread.join();
 
         Map.Entry<Integer, Integer> mostFrequentEntry = sizeToFreq.entrySet().stream()
                 .max(Map.Entry.comparingByValue())
@@ -25,24 +68,6 @@ public class Main {
                 }
             });
         }
-    }
-
-    private static ThreadGroup getThreadGroup() {
-        ThreadGroup group = new ThreadGroup("Group");
-
-        for (int i = 0; i < 1000; i++) {
-            Thread thread = new Thread(group, () -> {
-                String route = generateRoute("RLRFR", 100);
-                int repeats = countRInRoute(route);
-
-                synchronized (sizeToFreq) {
-                    sizeToFreq.merge(repeats, 1, Integer::sum);
-                }
-            });
-
-            thread.start();
-        }
-        return group;
     }
 
     public static String generateRoute(String letters, int length) {
